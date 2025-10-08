17828c20bd48e50d753f4a5b6a5031274db43f26
fuyutarow <fuyutarow@gmail.com>
2025-10-09 06:27:30 +0900

    feat: Add generated files for Walrus system, state, upgrade, and context
    - Introduced `system.ts` to define the `System` struct and various functions for blob management, storage reservations, and event certification.
    - Created `system_state_inner.ts` to define `SystemStateInnerV1` struct, encapsulating committee and capacity details.
    - Added `upgrade.ts` for managing contract upgrades, including voting, authorizing, and committing upgrades.
    - Implemented `walrus_context.ts` to encapsulate the current state of the Walrus system, improving readability and testing.

---
+++ sui/src/generated/walrus/committee.ts
@@ -0,0 +1,106 @@
+/**************************************************************
+ * THIS FILE IS GENERATED AND SHOULD NOT BE MANUALLY MODIFIED *
+ **************************************************************/
+
+/**
+ * This module defines the `Committee` struct which stores the current committee
+ * with shard assignments. Additionally, it manages transitions / transfers of
+ * shards between committees with the least amount of changes.
+ */
+
+import { bcs } from "@mysten/sui/bcs";
+import { type Transaction } from "@mysten/sui/transactions";
+import { MoveTuple, normalizeMoveArguments, type RawTransactionArgument } from "../utils/index";
+import * as vec_map from "./deps/sui/vec_map";
+
+const $moduleName = "Walrus::committee";
+export const Committee = new MoveTuple({
+  name: `${$moduleName}::Committee`,
+  fields: [vec_map.VecMap(bcs.Address, bcs.vector(bcs.u16()))],
+});
+export interface ShardsArguments {
+  cmt: RawTransactionArgument<string>;
+  nodeId: RawTransactionArgument<string>;
+}
+export interface ShardsOptions {
+  package?: string;
+  arguments:
+    | ShardsArguments
+    | [cmt: RawTransactionArgument<string>, nodeId: RawTransactionArgument<string>];
+}
+/** Get the shards assigned to the given `node_id`. */
+export function shards(options: ShardsOptions) {
+  const packageAddress = options.package ?? "Walrus";
+  const argumentsTypes = [
+    `${packageAddress}::committee::Committee`,
+    "0x0000000000000000000000000000000000000000000000000000000000000002::object::ID",
+  ] satisfies string[];
+  const parameterNames = ["cmt", "nodeId"];
+  return (tx: Transaction) =>
+    tx.moveCall({
+      package: packageAddress,
+      module: "committee",
+      function: "shards",
+      arguments: normalizeMoveArguments(options.arguments, argumentsTypes, parameterNames),
+    });
+}
+export interface SizeArguments {
+  cmt: RawTransactionArgument<string>;
+}
+export interface SizeOptions {
+  package?: string;
+  arguments: SizeArguments | [cmt: RawTransactionArgument<string>];
+}
+/** Get the number of nodes in the committee. */
+export function size(options: SizeOptions) {
+  const packageAddress = options.package ?? "Walrus";
+  const argumentsTypes = [`${packageAddress}::committee::Committee`] satisfies string[];
+  const parameterNames = ["cmt"];
+  return (tx: Transaction) =>
+    tx.moveCall({
+      package: packageAddress,
+      module: "committee",
+      function: "size",
+      arguments: normalizeMoveArguments(options.arguments, argumentsTypes, parameterNames),
+    });
+}
+export interface InnerArguments {
+  cmt: RawTransactionArgument<string>;
+}
+export interface InnerOptions {
+  package?: string;
+  arguments: InnerArguments | [cmt: RawTransactionArgument<string>];
+}
+/** Get the inner representation of the committee. */
+export function inner(options: InnerOptions) {
+  const packageAddress = options.package ?? "Walrus";
+  const argumentsTypes = [`${packageAddress}::committee::Committee`] satisfies string[];
+  const parameterNames = ["cmt"];
+  return (tx: Transaction) =>
+    tx.moveCall({
+      package: packageAddress,
+      module: "committee",
+      function: "inner",
+      arguments: normalizeMoveArguments(options.arguments, argumentsTypes, parameterNames),
+    });
+}
+export interface ToInnerArguments {
+  cmt: RawTransactionArgument<string>;
+}
+export interface ToInnerOptions {
+  package?: string;
+  arguments: ToInnerArguments | [cmt: RawTransactionArgument<string>];
+}
+/** Copy the inner representation of the committee. */
+export function toInner(options: ToInnerOptions) {
+  const packageAddress = options.package ?? "Walrus";
+  const argumentsTypes = [`${packageAddress}::committee::Committee`] satisfies string[];
+  const parameterNames = ["cmt"];
+  return (tx: Transaction) =>
+    tx.moveCall({
+      package: packageAddress,
+      module: "committee",
+      function: "to_inner",
+      arguments: normalizeMoveArguments(options.arguments, argumentsTypes, parameterNames),
+    });
+}
---
+++ sui/src/generated/walrus/init.ts
@@ -0,0 +1,112 @@
+/**************************************************************
+ * THIS FILE IS GENERATED AND SHOULD NOT BE MANUALLY MODIFIED *
+ **************************************************************/
+
+import { bcs } from "@mysten/sui/bcs";
+import { type Transaction } from "@mysten/sui/transactions";
+import { MoveStruct, normalizeMoveArguments, type RawTransactionArgument } from "../utils/index";
+import * as object from "./deps/sui/object";
+import * as _package from "./deps/sui/package";
+
+const $moduleName = "Walrus::init";
+export const INIT = new MoveStruct({
+  name: `${$moduleName}::INIT`,
+  fields: {
+    dummy_field: bcs.bool(),
+  },
+});
+export const InitCap = new MoveStruct({
+  name: `${$moduleName}::InitCap`,
+  fields: {
+    id: object.UID,
+    publisher: _package.Publisher,
+  },
+});
+export interface InitializeWalrusArguments {
+  initCap: RawTransactionArgument<string>;
+  upgradeCap: RawTransactionArgument<string>;
+  epochZeroDuration: RawTransactionArgument<number | bigint>;
+  epochDuration: RawTransactionArgument<number | bigint>;
+  nShards: RawTransactionArgument<number>;
+  maxEpochsAhead: RawTransactionArgument<number>;
+}
+export interface InitializeWalrusOptions {
+  package?: string;
+  arguments:
+    | InitializeWalrusArguments
+    | [
+        initCap: RawTransactionArgument<string>,
+        upgradeCap: RawTransactionArgument<string>,
+        epochZeroDuration: RawTransactionArgument<number | bigint>,
+        epochDuration: RawTransactionArgument<number | bigint>,
+        nShards: RawTransactionArgument<number>,
+        maxEpochsAhead: RawTransactionArgument<number>,
+      ];
+}
+/**
+ * Initializes Walrus and shares the system and staking objects.
+ *
+ * This can only be called once, after which the `InitCap` is destroyed.
+ */
+export function initializeWalrus(options: InitializeWalrusOptions) {
+  const packageAddress = options.package ?? "Walrus";
+  const argumentsTypes = [
+    `${packageAddress}::init::InitCap`,
+    "0x0000000000000000000000000000000000000000000000000000000000000002::package::UpgradeCap",
+    "u64",
+    "u64",
+    "u16",
+    "u32",
+    "0x0000000000000000000000000000000000000000000000000000000000000002::clock::Clock",
+  ] satisfies string[];
+  const parameterNames = [
+    "initCap",
+    "upgradeCap",
+    "epochZeroDuration",
+    "epochDuration",
+    "nShards",
+    "maxEpochsAhead",
+  ];
+  return (tx: Transaction) =>
+    tx.moveCall({
+      package: packageAddress,
+      module: "init",
+      function: "initialize_walrus",
+      arguments: normalizeMoveArguments(options.arguments, argumentsTypes, parameterNames),
+    });
+}
+export interface MigrateArguments {
+  staking: RawTransactionArgument<string>;
+  system: RawTransactionArgument<string>;
+}
+export interface MigrateOptions {
+  package?: string;
+  arguments:
+    | MigrateArguments
+    | [staking: RawTransactionArgument<string>, system: RawTransactionArgument<string>];
+}
+/**
+ * Migrates the staking and system objects to the new package ID.
+ *
+ * This must be called in the new package after an upgrade is committed to emit an
+ * event that informs all storage nodes and prevent previous package versions from
+ * being used.
+ *
+ * Requires the migration epoch to be set first on the staking object, which then
+ * enables the migration at the start of the next epoch.
+ */
+export function migrate(options: MigrateOptions) {
+  const packageAddress = options.package ?? "Walrus";
+  const argumentsTypes = [
+    `${packageAddress}::staking::Staking`,
+    `${packageAddress}::system::System`,
+  ] satisfies string[];
+  const parameterNames = ["staking", "system"];
+  return (tx: Transaction) =>
+    tx.moveCall({
+      package: packageAddress,
+      module: "init",
+      function: "migrate",
+      arguments: normalizeMoveArguments(options.arguments, argumentsTypes, parameterNames),
+    });
+}
